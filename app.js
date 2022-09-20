d9405eb8368a56685aede98df688217300c6d84c
coco910904 <103620530+coco910904@users.noreply.github.com>
2022-09-20 18:12:05 +0800

    '20220920修正聊天bug'

--- app.js
+++ app.js
@@ -3,21 +3,39 @@ const app = Vue.createApp({
 		return {
 			enteredContentValue: '',
 			computerContentValue: '',
-			messagedatas: [],
+			messagedatas: [
+				{
+					username: true,
+					img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/3364143/download+%281%29.png',
+					content: 'I say you say ',
+				},
+				{
+					username: false,
+					img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/3364143/download+%283%29+%281%29.png',
+					content: 'I say you say ',
+				},
+			],
 		};
 	},
 	methods: {
 		addMessage() {
-			const dataOwer = {
-				username: true,
-				img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/3364143/download+%281%29.png',
-				content: this.enteredContentValue,
-			};
-			this.messagedatas.unshift(dataOwer);
-			this.computerContentValue = this.enteredContentValue;
-			this.enteredContentValue = '';
-			this.awaitMessage();
-			console.log(this.messagedatas);
+			if (
+				this.enteredContentValue.replace(/(^s*)|(s*$)/g, '').length !==
+				0
+			) {
+				const dataOwer = {
+					username: true,
+					img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/3364143/download+%281%29.png',
+					content: this.enteredContentValue,
+				};
+				this.messagedatas.push(dataOwer);
+				this.computerContentValue = this.enteredContentValue;
+				this.enteredContentValue = '';
+				this.awaitMessage();
+				console.log(this.messagedatas);
+			} else {
+				alert('請輸入文字');
+			}
 		},
 		awaitMessage() {
 			const that = this;
@@ -32,7 +50,7 @@ const app = Vue.createApp({
 			});
 			awaitValue
 				.then(res => {
-					this.messagedatas.unshift(res);
+					this.messagedatas.push(res);
 					console.log(res);
 					console.log(this.messagedatas);
 					this.computerContentValue = '';
