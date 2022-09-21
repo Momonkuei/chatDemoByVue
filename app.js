33f04cf71ae60d77247fae55de1dd9d62d99e79c
coco910904 <103620530+coco910904@users.noreply.github.com>
2022-09-21 15:11:40 +0800

    '20220921自動到底完成'

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const app = Vue.createApp({
+	name: 'scroll',
 	data() {
 		return {
 			enteredContentValue: '',
@@ -12,7 +13,7 @@ const app = Vue.createApp({
 				{
 					username: false,
 					img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/3364143/download+%283%29+%281%29.png',
-					content: 'I say you say ',
+					content: 'You say i say ',
 				},
 			],
 		};
@@ -32,7 +33,12 @@ const app = Vue.createApp({
 				this.computerContentValue = this.enteredContentValue;
 				this.enteredContentValue = '';
 				this.awaitMessage();
-				console.log(this.messagedatas);
+				// console.log(this.messagedatas);
+				this.$nextTick(() => {
+					let msg = document.getElementById('chatArea');
+					msg.scrollTop = msg.scrollHeight;
+					console.log(msg);
+				});
 			} else {
 				alert('請輸入文字');
 			}
@@ -46,7 +52,7 @@ const app = Vue.createApp({
 						img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/3364143/download+%283%29+%281%29.png',
 						content: that.computerContentValue,
 					});
-				}, 1500);
+				}, 1000);
 			});
 			awaitValue
 				.then(res => {
@@ -54,10 +60,30 @@ const app = Vue.createApp({
 					console.log(res);
 					console.log(this.messagedatas);
 					this.computerContentValue = '';
+					this.$nextTick(() => {
+						let msg = document.getElementById('chatArea');
+						msg.scrollTop = msg.scrollHeight;
+						console.log(msg);
+					});
 				})
 				.catch(err => console.log(err));
 		},
+		scrollToEnd() {
+			var content = this.$refs.container;
+			content.scrollTop = content.scrollHeight;
+		},
 	},
+	// updated() {
+	// 	// This will be called when the component updates
+	// 	// try toggling a todo
+	// 	this.scrollToEnd();
+	// },
+
+	// mounted() {
+	// 	// This will be called on load
+	// 	this.scrollToEnd();
+	// },
 });
 
 app.mount('#chatArea');
+app.use(VueSmoothScroll);
